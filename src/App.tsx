58f9f5fb9e4b374f27b396fe48a4f57bfb935663
KrisKnez <kristijankej@gmail.com>
2023-04-22 12:53:47 +0200

    Nova struktura glavne komponente

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-
 import './App.css'
-
+ 
 function App() {
-
-
+ 
   return (
-    <>
-  
-    </>
+    <div className="App">
+      <h1>Uvod u git</h1>
+      <p>Praćenje promjena u repozitoriju</p>
+    </div>
   )
 }
-
+ 
 export default App
